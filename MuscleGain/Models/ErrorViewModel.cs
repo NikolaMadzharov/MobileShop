3ba9a268ba4727956f6f45924cc88bff5dba7856
Nikola Madzharov <89745007+koko1912@users.noreply.github.com>
2022-10-05 23:08:18 +0300

    Delete MuscleGain directory

--- MuscleGain/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MuscleGain.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
